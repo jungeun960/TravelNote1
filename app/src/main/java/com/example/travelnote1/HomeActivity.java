4785acf2635a8de32a62e9d615e5851ddc3917ce
jungeun960 <wjddms905@gmail.com>
2019-11-13 20:09:24 +0900

    여행 - 진행중

--- app/src/main/java/com/example/travelnote1/HomeActivity.java
+++ app/src/main/java/com/example/travelnote1/HomeActivity.java
@@ -2,6 +2,7 @@ package com.example.travelnote1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
@@ -24,13 +25,9 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        String travel_title = "";
-        String travel_img = "";
-
-        Bundle extras = getIntent().getExtras();
-
-        travel_title = extras.getString("travel_title");
-        travel_img = extras.getString("travel_img");
+        Intent intent = getIntent();
+        String travel_title = intent.getStringExtra("travel_title");
+        String travel_img = intent.getStringExtra("travel_img");
         Log.d("값 확인","제목 : " +travel_title + " 이미지 uri : " + travel_img);
 
         TextView title = (TextView) findViewById(R.id.title);
@@ -38,17 +35,19 @@ public class HomeActivity extends AppCompatActivity {
 
         title.setText(travel_title);
         //Glide.with(this).load(travel_img).into(img);
+        Glide.with(this).load("content://com.android.providers.media.documents/document/image%3A179738").into(img);
         //Glide를 이용하여 이미지뷰에 url에 있는 이미지를 세팅해줌
 
-        try {
-            Uri uri = Uri.parse(travel_img);
-            Log.e("uri로 변경","ff");
-            InputStream in = getContentResolver().openInputStream(uri);
-            Bitmap imga = BitmapFactory.decodeStream(in);
-            in.close();
-            img.setImageBitmap(imga);
-        } catch (Exception e) {
+
+//        try {
+//
+//            Log.e("uri로 변경","ff");
+//            InputStream in = getContentResolver().openInputStream(uri);
+//            Bitmap imga = BitmapFactory.decodeStream(in);
+//            in.close();
+//            img.setImageBitmap(imga);
+//        } catch (Exception e) {
 
        //img.setImageResource(travel_img);
     }
-}}
+}
